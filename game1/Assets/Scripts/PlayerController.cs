30b99adf62a9fb17e01824f9c122b8230d82e0bb
t-shira <t.a.711.ssw@gmail.com>
2018-09-30 21:03:26 +0900

    game1 fix

---
+++ game1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        count = 0;
+
+        winText.text = "";
+
+        SetCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+
+    void SetCountText() 
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12) 
+        {
+            winText.text = "You win!";
+        }
+    }
+}
